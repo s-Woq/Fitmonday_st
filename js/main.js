5d9ebaf290dcb8175af76b680b0782ac1394b6d6
midward <midwardisaac@gmail.com>
2023-03-05 12:02:36 -0500

    Added counter and modified it's design

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+dayjs.extend(dayjs_plugin_duration);
 
+function activateCountdown(element,dayString){
+    const targetDate=dayjs(dayString);
+
+    document.querySelector(".until__event").textContent = `Until${targetDate.format (" DD MMMM YYYY")}`;
     
+
+    setInterval(()=>{
+        const now = dayjs();
+        const duration= dayjs.duration(targetDate.diff(now));
+
+        document.querySelector(".until__numeric--seconds").textContent = duration.seconds().toString().padStart(2,"0")
+        document.querySelector(".until__numeric--minutes").textContent = duration.minutes().toString().padStart(2,"0")
+        document.querySelector(".until__numeric--hours").textContent = duration.hours().toString().padStart(2,"0")
+        document.querySelector(".until__numeric--days").textContent = duration.asDays().toFixed(0).toString().padStart(2,"0")
+    }, 250)
+
+}
+
+activateCountdown(document.getElementById("myCountdown"), "2023-04-05")
